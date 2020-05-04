30f504020fb9395ba321cab511ebff1db0a3aca6
Alex Bloom <lehanbloom@gmail.com>
2020-05-04 20:16:34 +0300

    Day 2. Completed ex.3, which was to add a nested route to the Product page so that when user clicks price, the price for the product is displated.

--- day-2-exercises/src/App.js
+++ day-2-exercises/src/App.js
@@ -4,7 +4,8 @@ import CompanyProducts from "./components/CompanyProducts";
 import ProductPage from "./components/ProductPage";
 import List from "./components/List";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/App.css";
 
 class App extends React.Component {
   constructor(props) {
@@ -16,21 +17,33 @@ class App extends React.Component {
           id: 1,
           name: "Windows OS",
           logo: "/images/windows_os.jpg",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+          price: "$1000",
         },
         {
           id: 2,
           name: "Microsoft Office",
           logo: "/images/Office.png",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+          price: "$350",
         },
         {
           id: 3,
           name: "Windows Phone",
           logo: "/images/w_phone.jpg",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+          price: "$545.99",
         },
         {
           id: 4,
           name: "Visual Studio",
           logo: "/images/vs_studio.jpg",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+          price: "$150",
         },
       ],
     };
@@ -41,12 +54,14 @@ class App extends React.Component {
     return (
       <div className="App">
         <Router>
-          <Link to="/" className="page-links">
-            Home
-          </Link>
-          <Link to="/products" className="page-links">
-            Company Products
-          </Link>
+          <div className="nav">
+            <Link to="/" className="page-links">
+              Home
+            </Link>
+            <Link to="/products" className="page-links">
+              Products
+            </Link>
+          </div>
           <Switch>
             <Route path="/product/:productid">
               <ProductPage products={company_products}></ProductPage>
--- day-2-exercises/src/components/ProductPage.jsx
+++ day-2-exercises/src/components/ProductPage.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-import { withRouter } from "react-router-dom";
+import { withRouter, Switch, Route, Link } from "react-router-dom";
+import "../css/Product.css";
 
 class ProductPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      product: { name: "The product does not exist.", logo: null },
+      product: {
+        name: "The product does not exist.",
+        logo: null,
+        id: null,
+        description: null,
+        price: null,
+      },
     };
   }
 
@@ -28,12 +35,24 @@ class ProductPage extends React.Component {
 
   render() {
     const { product } = this.state;
+    const { match } = this.props;
+    const urlPrice = match.params.productid + "/price";
+    console.log(urlPrice);
     return (
       <>
         {product && (
           <div>
-            <h1>{product.name}</h1>
+            <h1 className="product-name">{product.name}</h1>
             <img src={product.logo} alt={product.name} />
+            <p className="product-description">{product.description}</p>
+            <Link to={urlPrice} className="price-link">
+              <button className="btn btn-success">Price</button>
+            </Link>
+            <Switch>
+              <Route path={`/product/:productid/price`}>
+                <p>{product.price}</p>
+              </Route>
+            </Switch>
           </div>
         )}
       </>
